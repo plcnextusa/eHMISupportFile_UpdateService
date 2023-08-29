f21a2f2b04b368636a9fefb0c16126e3a0249327
dclark3774 <55795713+dclark3774@users.noreply.github.com>
2023-08-29 09:22:27 -0600

    Add files via upload

--- updateFileCheck.sh
+++ updateFileCheck.sh
@@ -3,36 +3,39 @@
 # Path definitions
 BASE_PATH=/opt/plcnext/
 EHMI_PATH=/opt/plcnext/projects/PCWE/Services/Ehmi/ehmi/
+LOGFILE=/opt/plcnext/logs/updates.log
 
+# Loop Counter
+index=0
 
 # Function to check for tar file and extract if exists
-function fileExists() {
+function fileExists(idx) {
   # Check for update file
   if [ -f ${EHMI_PATH}update.tar ]; then
     # Extract update file
     tar -xf ${EHMI_PATH}update.tar -C /opt/plcnext/
     # Check for script to execute update
     if [ -f ${BASE_PATH}update/updateFiles.sh ]; then
-      echo "Update tar found. Installing file updates."
+      echo "$(date +%F_%T) - Update tar found. Installing file updates.\n" >> LOGFILE
       return 0
     else
-      echo "Update tar found but no update script attached."
+      echo "$(date +%F_%T) - Update tar found but no update script attached.\n" >> LOGFILE
       return 1
     fi
   else
     # Update file does not exist.
-    echo "Update does not exist"
     return 1
   fi
 }
 
+function
 
 
 # Loop to run forever
 while [ 1 ]
 do
   # Only run script if file exists and extract is successful.
-  if fileExists; then
+  if fileExists(index); then
     sh ${BASE_PATH}update/updateFiles.sh
     rm -r ${EHMI_PATH}update.tar
   fi
